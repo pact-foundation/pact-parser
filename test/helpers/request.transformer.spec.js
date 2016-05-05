60a4fe3fc9945cdd7e41a336d6438319da49b325
ichyr <zmii@plast.org.ua>
2016-05-05 02:20:34 +0200

    updates test for helper

--- test/helpers/request.transformer.spec.js
+++ test/helpers/request.transformer.spec.js
@@ -7,7 +7,7 @@ describe('Helpers', function () {
   describe('RequestTransformer', function () {
     var request = {
       method: 'GET',
-      url: '/some/url',
+      path: '/some/url',
       query: { test: 12 },
       headers: { test: 12 },
       body: { test: 12 }
@@ -17,7 +17,7 @@ describe('Helpers', function () {
       var e = request; 
       var actual = RequestTransformer(request);
       expect(actual.method).to.equal(e.method);
-      expect(actual.path).to.equal(e.url);
+      expect(actual.path).to.equal(e.path);
       expect(actual.query).to.deep.equal(e.query);
       expect(actual.headers).to.deep.equal(e.headers);
       expect(actual.body).to.deep.equal(e.body);
