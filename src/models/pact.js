1048d2a56da8cfd73c4061109d0e5e3c7ac74a09
ichyr <zmii@plast.org.ua>
2016-05-05 03:24:49 +0200

    resolved eslint problems

--- src/models/pact.js
+++ src/models/pact.js
@@ -3,22 +3,22 @@
 var Interaction = require('./interaction');
 
 function Pact(options) {
-	this.provider = options.provider;
-	this.consumer = options.consumer;
-	this.interactions = options.interactions.map(
-		function (interaction) {
-			return new Interaction(interaction);
-		}
-	);
-	this.metadata = options.metadata;
+  this.provider = options.provider;
+  this.consumer = options.consumer;
+  this.interactions = options.interactions.map(
+    function (interaction) {
+      return new Interaction(interaction);
+    }
+  );
+  this.metadata = options.metadata;
 }
 
 Pact.prototype.match = function (request) {
-	var result, i;
-	for (i = 0; i < this.interactions.length; i++) {
-		  result = this.interactions[i].match(request);
-		  if (result) return result;
-	}
+  var result, i;
+  for (i = 0; i < this.interactions.length; i++) {
+    result = this.interactions[i].match(request);
+    if (result) return result;
+  }
   return result;
 };
 
--- test/models/pact.collection.spec.js
+++ test/models/pact.collection.spec.js
@@ -2,65 +2,64 @@
 
 var expect = require('chai').expect,
   PactCollection = require('../../src/models/pact.collection.js'),
-  Pact = require('../../src/models/pact.js'),
   PactMock = require('./mocks/pact.mock.js');
 
 
 describe('Models', function () {
 
-		                    describe('PactCollection', function () {
+  describe('PactCollection', function () {
 
-  it('should be defined', function () {
-    expect(PactCollection).to.be.a('function');
-  });
-
-
-  describe(' public API should contain', function () {
-    var instance,
-      pactFiles = [PactMock];
-
-    beforeEach(function () {
-      instance = new PactCollection(pactFiles);
+    it('should be defined', function () {
+      expect(PactCollection).to.be.a('function');
     });
 
 
-    it('.pacts', function () {
-      expect(instance.pacts).to.be.a('array');
-    });
+    describe(' public API should contain', function () {
+      var instance,
+        pactFiles = [PactMock];
 
-    it('.match', function () {
-      expect(instance.match).to.be.a('function');
-    });
-
-  });
-
-
-  describe('public API', function () {
-
-    var instance,
-      pactFiles = [PactMock];
-
-    beforeEach(function () {
-      instance = new PactCollection(pactFiles);
-    });
-
-    describe('.match', function () {
-
-      it('should return undefined if no request matches', function () {
-        expect(instance.match({ method: 'DELETE' })).to.equal(undefined);
+      beforeEach(function () {
+        instance = new PactCollection(pactFiles);
       });
 
 
-      it('should return response of the matching request', function () {
-        var temp = PactMock.interactions[0],
-          res = instance.match(temp.request);
-        expect(res.status).to.equal(temp.response.status);
+      it('.pacts', function () {
+        expect(instance.pacts).to.be.a('array');
+      });
+
+      it('.match', function () {
+        expect(instance.match).to.be.a('function');
       });
 
     });
 
+
+    describe('public API', function () {
+
+      var instance,
+        pactFiles = [PactMock];
+
+      beforeEach(function () {
+        instance = new PactCollection(pactFiles);
+      });
+
+      describe('.match', function () {
+
+        it('should return undefined if no request matches', function () {
+          expect(instance.match({ method: 'DELETE' })).to.equal(undefined);
+        });
+
+
+        it('should return response of the matching request', function () {
+          var temp = PactMock.interactions[0],
+            res = instance.match(temp.request);
+          expect(res.status).to.equal(temp.response.status);
+        });
+
+      });
+
+    });
+
+
   });
-
-
-});
 });
