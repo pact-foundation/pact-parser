48e237be100d7b0d1f9ba600422917a036907bae
ichyr <zmii@plast.org.ua>
2016-05-13 23:42:14 +0200

    updates packege,json dependencies. moved server spawn loin into main.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var argv = require('yargs').argv,
   glob = require('glob'),
   path = require('path'),
   pactFiles = [],
-  pacts = [];
+  PactParser = require('./src/main.js');
 
 if (!argv.path) {
   throw new Error('Please specify path to pact files with `--path <path_to_files>` argument.');
@@ -15,32 +15,6 @@ glob.sync(argv.path).forEach(function (file) {
   pactFiles.push(require(path.resolve(file)));
 });
 
-// Digest the pact files into array of pact objects
-var PactCollection = require('./src/models/pact.collection.js');
-pacts = new PactCollection(pactFiles);
-
-// Spawn the Node server
-var pactConfig = require('./pact.config.json');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.json());
-
-function pactChecker(req, res) {
-  var response = pacts.match(req);
-  if (response) {
-    res.status(response.status).send(response.body);  
-  } else {
-    res.status(500).send('No interaction found for your request');
-  }  
-}
-
-app.use(pactChecker);
-
-/*eslint no-console: 0 */
-
-app.listen(argv.port || pactConfig.PORT, function () {
-  console.log('Server listening on: http://localhost:%s', argv.port || pactConfig.PORT);
-});
+PactParser(pactFiles, argv.port);
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 'use strict';
 
-var argv = require('yargs').argv;
+var pactConfig = require('../pact.config.json'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  PactCollection = require('./models/pact.collection.js');
 
-if (!argv.path) {
-  throw new Error('Please specify path to pact files with `--path <path_to_files>` argument.');
-}
+function PactParser(pactFiles, port) {
+  // Digest the pact files into array of pact objects
+  var pacts = new PactCollection(pactFiles);
 
+  // Spawn the Node server  
+  app.use(bodyParser.json());
+
+  function pactChecker(req, res) {
+    var response = pacts.match(req);
+    if (response) {
+      res.status(response.status).send(response.body);
+    } else {
+      res.status(500).send('No interaction found for your request');
+    }
+  }
+
+  app.use(pactChecker);
+
+  /*eslint no-console: 0 */
+
+  var startPort = port || pactConfig.PORT;
+  return app.listen(startPort, function () {
+    console.log('Server listening on: http://localhost:%s', startPort);
+  });
+} 
+
+module.exports = PactParser;
